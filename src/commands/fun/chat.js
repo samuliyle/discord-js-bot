e43def708035fb876cac0dbf804152347292875f
Samuli Ylenius <wraithyzz@gmail.com>
2017-03-15 10:48:30 +0200

    switched cleverbot wrapper so it will remember previous interactions

--- src/commands/fun/chat.js
+++ src/commands/fun/chat.js
@@ -1,24 +1,25 @@
-const Cleverbot = require('cleverbot');
+const Cleverbot = require('cleverbot-node');
 const Promise = require('bluebird');
 
 const constants = require('../../../config/constants.js');
 
-const cleverbot = new Cleverbot({
-  key: constants.CLEVERBOT
-});
+const cleverbot = new Cleverbot();
+cleverbot.configure({botapi: constants.CLEVERBOT});
 
 function chat(message) {
-  if (message.length === 0) return (Promise.resolve('Yes...?'));
+  if (message.length === 0)
+    return (Promise.resolve('Yes...?'));
   const msg = message.join(' ');
+  console.log(msg);
   return new Promise((resolve, reject) => {
-    cleverbot.query(msg)
-      .then((response) => {
-        resolve(response.output);
-      });
+    cleverbot.write(msg, (response) => {
+      console.log(response);
+      resolve(response.output);
+    });
   });
 }
 
 module.exports = {
   mörkö: chat,
-  morko: chat,
+  morko: chat
 };
