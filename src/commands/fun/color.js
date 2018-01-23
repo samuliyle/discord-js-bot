eabefd022a2ab752304d774e222079028e92f0ba
Samuli Ylenius <wraithyzz@gmail.com>
2018-01-23 12:12:23 +0200

    colorloop

--- src/commands/fun/color.js
+++ src/commands/fun/color.js
@@ -2,7 +2,9 @@ const _ = require('lodash');
 const Promise = require('bluebird');
 const parse = require('parse-color');
 
-function randomColor(parameters, message) {
+const colorLoops = [];
+
+function randomColor(parameters, message, print = true) {
     const { author, member } = message;
     if (!_.isNil(author) && !_.isNil(member)) {
         let colorHex = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6); 
@@ -28,7 +30,9 @@ function randomColor(parameters, message) {
                         .member
                         .addRole(r)
                         .then(() => {
-                            message.channel.send(`Set color to http://garden.offbeatwit.ch/color/${colorHex.replace('#', '')}`);
+                            if (print) {
+                                message.channel.send(`Set color to http://garden.offbeatwit.ch/color/${colorHex.replace('#', '')}`);
+                            }
                             return Promise.resolve();
                         });
                 })
@@ -43,7 +47,9 @@ function randomColor(parameters, message) {
                     color: colorHex
                 })
                 .then(() => {
-                    message.channel.send(`Set color to http://garden.offbeatwit.ch/color/${colorHex.replace('#', '')}`);
+                    if (print) {
+                        message.channel.send(`Set color to http://garden.offbeatwit.ch/color/${colorHex.replace('#', '')}`);
+                    }
                     return Promise.resolve();
                 })
                 .catch((e) => {
@@ -54,7 +60,38 @@ function randomColor(parameters, message) {
     Promise.resolve();
 }
 
+function colorLoop(parameters, message) {
+    const exists = _.find(colorLoops, (a) => {
+        return message.author.id === a.message.author.id && message.guild.id === a.message.guild.id;
+    });
+    if (!_.isNil(exists)) {
+        clearInterval(exists.interval);
+        const index = colorLoops.indexOf(exists);
+        if (index !== -1) {
+            colorLoops.splice(index, 1);
+        }
+        return Promise.resolve('Color loop stopped');
+    }
+    let time = 3600000; // Hour
+    if (_.size(parameters) !== 0) {
+        const parsedTime = _.parseInt(parameters[0]);
+        if (!_.isNaN(parsedTime) && parsedTime >= 1 && parsedTime < 10080) {
+            time = parsedTime * 60000;
+        }
+    }
+    randomColor([], message, false);
+    const loop = setInterval(() => {
+        randomColor([], message, false);
+    }, time);
+    colorLoops.push({
+        message,
+        interval: loop
+    });
+    return Promise.resolve(`Color loop started. Changing color every ${time / 60000} minutes.`);
+}
+
 module.exports = {
     color: randomColor,
-    randomColor
+    randomColor,
+    colorloop: colorLoop
 };
